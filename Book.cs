1bd44d0e996513ec6679611f62136393e6c48e21
AxZoRos <zenjakonovalov154@gmail.com>
2025-09-05 01:16:21 +0300

    add project
    Додано проєкт

---
+++ Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB12_KTP
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public int YearPublished;
+        public double Price;
+        // Параметричний конструктор
+        public Book(string title, string author, int yearPublished, double price)
+        {
+            Title = title;
+            Author = author;
+            YearPublished = yearPublished;
+            Price = price;
+        }
+        // Метод для виведення інформації
+        public void DisplayBookInfo()
+        {
+            Console.WriteLine($"Назва: {Title}, Автор: {Author}, Рік видання: {YearPublished}, Ціна: {Price} грн.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB12_KTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; 
+            Book book = new Book("The Power of Now: A Guide to Spiritual Enlightenment", "Екхарт Толле", 2017, 415);
+            book.DisplayBookInfo();// Метод для виводу інформації про книгу.
+            Console.ReadLine();
+        }
+    }
+}
